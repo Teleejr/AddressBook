c2de53ffaf3a41664d8582460c38e8b1fd1ea5c9
Teleejr <81259920+Teleejr@users.noreply.github.com>
2021-04-13 18:46:39 -0400

    Print all contacts to console. Unsure if I want to keep the field designation or not.

--- src/main/java/MainRunner.java
+++ src/main/java/MainRunner.java
@@ -1,6 +1,29 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class MainRunner {
 
     public static void main(String[] args) {
+        File fileName = new File("src/main/resources/Address.csv");
+        List<String> contacts = null;
+        Scanner scan;
+
+        try {
+            scan = new Scanner(fileName);
+            contacts = new ArrayList<>();
+
+            while(scan.hasNextLine()) {
+                contacts.add(scan.nextLine());
+            }//End while
+
+        }//End try
+        catch (FileNotFoundException f) {
+            f.printStackTrace();
+        }//End catch
+        for (String contact : contacts) System.out.println(contact);
+
 
     }
 }
