2c9c892c572f53f683c42d05e779b1ffbc9125ad
loitrem <ski_fix@hotmail.com>
2021-04-13 17:33:49 -0400

    add address to file working

--- src/main/java/MainRunner.java
+++ src/main/java/MainRunner.java
@@ -1,6 +1,42 @@
+import java.util.Scanner;
+
 public class MainRunner {
 
     public static void main(String[] args) {
-    test
+
+        // enable scanner for input
+        Scanner input = new Scanner(System.in);
+
+        //instantiate person class to call add method
+        Person p = new Person();
+
+        // array to tell user what to enter
+        String[] prompts = new String[9];
+        prompts[0] = "enter First Name";
+        prompts[1] = "enter Last Name";
+        prompts[2] = "enter Home Phone";
+        prompts[3] = "enter Mobile Phone";
+        prompts[4] = "enter Email";
+        prompts[5] = "enter Street";
+        prompts[6] = "enter Street Number";
+        prompts[7] = "enter Town";
+        prompts[8] = "enter Zip";
+
+        // array to save user input
+        String[] add = new String[9];
+
+        // display text from array and collect responses in array
+        for (int i=0; prompts.length>i; i++)
+        {
+            System.out.println(prompts[i]);
+            add[i] = input.nextLine();
+        }
+
+        // called add method to add user responses to file
+        p.add(add[0], add[1], add[2], add[3], add[4], add[5], add[6], add[7], add[8]);
+
+
+
+
     }
 }
