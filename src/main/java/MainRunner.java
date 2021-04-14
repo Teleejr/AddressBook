2c8e2855d8c3cdd401db1db19b9b5b5c594312dc
loitrem <ski_fix@hotmail.com>
2021-04-14 14:02:50 -0400

    add feature works

--- src/main/java/MainRunner.java
+++ src/main/java/MainRunner.java
@@ -12,7 +12,7 @@ public class MainRunner {
 
 
         BookManager bm = new BookManager();
-        /*
+
         List<Person> contactList = new ArrayList<>();
         FileReader fr = null;
         try {
@@ -31,7 +31,7 @@ public class MainRunner {
             }
         }
 
-
+/*
         System.out.println("------------------------------------");
         System.out.println(Arrays.toString(contactList.toArray()));
         System.out.println("------------------------------------");
@@ -71,36 +71,7 @@ public class MainRunner {
             }
             else if (menuChoice==1) {
                 //have the user enter another address and add it to the file
-
-                // enable scanner for input
-                Scanner input = new Scanner(System.in);
-
-                // array to tell user what to enter
-                String[] prompts = new String[9];
-                prompts[0] = "enter First Name";
-                prompts[1] = "enter Last Name";
-                prompts[2] = "enter Home Phone";
-                prompts[3] = "enter Mobile Phone";
-                prompts[4] = "enter Email";
-                prompts[5] = "enter Street";
-                prompts[6] = "enter Street Number";
-                prompts[7] = "enter Town";
-                prompts[8] = "enter Zip";
-
-                // array to save user input
-                String[] add = new String[9];
-
-                // display text from array and collect responses in array
-                for (int i=0; prompts.length>i; i++)
-                {
-                    System.out.println(prompts[i]);
-                    add[i] = input.nextLine();
-                }
-
-                // called add method to add user responses to file
-                Person p = new Person(add[0], add[1], Long.parseLong(add[2]), Long.parseLong(add[3]), add[4],
-                        new Address(Integer.parseInt(add[6]), add[5], add[7], Integer.parseInt(add[8])));
-
+                bm.enterAdd();
                 menuChoice = 0;
             }
             else if (menuChoice==2)
--- src/main/java/org/perscholas/data/BookManager.java
+++ src/main/java/org/perscholas/data/BookManager.java
@@ -10,24 +10,34 @@ public class BookManager implements Add, Update<Person>, Search<Person> {
 
     //method to append users input into file
     public void enterAdd() {
+        // enable scanner for input
+        Scanner input = new Scanner(System.in);
 
+        // array to tell user what to enter
+        String[] prompts = new String[9];
+        prompts[0] = "enter First Name";
+        prompts[1] = "enter Last Name";
+        prompts[2] = "enter Home Phone";
+        prompts[3] = "enter Mobile Phone";
+        prompts[4] = "enter Email";
+        prompts[5] = "enter Street";
+        prompts[6] = "enter Street Number";
+        prompts[7] = "enter Town";
+        prompts[8] = "enter Zip";
+
+        // array to save user input
+        String[] add = new String[9];
+
+        // display text from array and collect responses in array
+        for (int i=0; prompts.length>i; i++)
+        {
+            System.out.println(prompts[i]);
+            add[i] = input.nextLine();
+        }
+
+        // called add method to add user responses to file
+        addAddress(add[0], add[1], add[2], add[3], add[4], add[5], add[6], add[7], add[8]);
     }
-
-    //update person by name
-    @Override
-    public Person update(String name, String element, String value) {
-       //insert search implementation
-        //write logic of update  funtion
-        return null;
-    }
-
-    //update person by phone
-    @Override
-    public Person update(long phone, String element, String value) {
-       //insert search implementation
-       return null;
-    }
-
 
     //search person by phone
     @Override
@@ -41,6 +51,16 @@ public class BookManager implements Add, Update<Person>, Search<Person> {
         return null;
     }
 
+    @Override
+    public Person update(String name) {
+        return null;
+    }
+
+    @Override
+    public Person update(long phone) {
+        return null;
+    }
+
 
     /**
      * Gets all contact from file.
@@ -49,11 +69,11 @@ public class BookManager implements Add, Update<Person>, Search<Person> {
      * @return the all contact from file
      * @throws IOException the io exception
      */
-   /* public List<Person> getAllContactFromFile(FileReader fr) throws IOException {
+   public List<Person> getAllContactFromFile(FileReader fr) throws IOException {
 
         BufferedReader br = new BufferedReader(fr);  //creates a buffering character input stream
         Person p = null;  // new object
-        List<Person> persons = new List<>();
+        List<Person> persons = new ArrayList<>();
         String line;  // String to hold content
 
         while ((line = br.readLine()) != null)  // while the line is not empty
@@ -66,7 +86,7 @@ public class BookManager implements Add, Update<Person>, Search<Person> {
             words = new String[9]; //remove data from arrays
         }
         return persons;
-    }*/
+    }
 
 
 }
