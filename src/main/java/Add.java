92414310e5a38b943332489dc7c27d1119213d7b
loitrem <ski_fix@hotmail.com>
2021-04-13 23:03:46 -0400

    add address to file working

---
+++ src/main/java/Add.java
@@ -0,0 +1,40 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public interface Add {
+
+
+    default void Add(String name, String surname, String phone, String mobile, String email, String street, String number, String town, String zip) {
+
+        //set access to file
+        File file = new File("src/main/resources/Address.csv");
+
+        BufferedWriter buffer = null;
+
+        //try to write to file if no exceptions
+        try {
+            FileWriter fileWriter = new FileWriter(file, true);
+            buffer = new BufferedWriter(fileWriter);
+            buffer.write(name.trim().toLowerCase() + "," + surname.trim().toLowerCase() + "," + phone.trim().toLowerCase() + "," +
+                    mobile.trim().toLowerCase() + "," + email.trim().toLowerCase() + "," + street.trim().toLowerCase() + "," +
+                    number.trim().toLowerCase() + "," + town.trim().toLowerCase() + "," + zip.trim().toLowerCase() + "\n");
+        }
+        // catch any exceptions
+        catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                //if the fileWriter isn't null flush and close it
+                if (buffer != null) {
+                    buffer.flush();
+                    buffer.close();
+                }
+                // catch any IO exceptions
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
